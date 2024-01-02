5535e0e88755f5a9269b61d5329438314ede3eb6
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2024-01-02 00:19:14 -0800

    entity config

---
+++ source/constants/constant_train.py
@@ -0,0 +1,15 @@
+TARGET_COLUMN = 'Churn'
+PIPELINE_NAME = 'training_pipeline'
+ARTIFACT_DIR = 'artifact'
+FILE_NAME = 'training_data.csv'
+
+TRAIN_FILE_NAME = 'train.csv'
+TEST_FILE_NAME = 'test.csv'
+
+MONGODB_URL_KEY = "mongodb+srv://amrutasharnangat2503:mongo1234@cluster0.r6fy3lw.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'db-customer-churn'
+DI_COLLECTION_NAME = 'telco-customer-churn'
+DI_DIR_NAME = 'data_ingestion'
+DI_FEATURE_STORE_DIR = 'feature_store'
+DI_INGESTED_DIR = 'ingested'
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
